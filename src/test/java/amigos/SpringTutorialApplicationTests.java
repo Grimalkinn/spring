42c55e05d6ef5514273beafa583b51a5c78eaccc
Grimalkinn <okwuoasfelix634@gmail.com.com>
2024-10-14 23:40:03 +0300

    first commit

---
+++ src/test/java/amigos/SpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package amigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
